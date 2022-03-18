0ab31554c1c8e83ee2e43d83822124ee19caa42b
Thaina <thschumann85@outlook.de>
2022-03-18 13:33:38 +0100

    commit

---
+++ index.js
@@ -0,0 +1,78 @@
+function playmusic(){
+  var audiopuma = new Audio();
+  audiopuma.src = "sound/PANTHER.mp3";
+  audiopuma.play();
+}
+
+function playmusic1(){
+  var audioalli = new Audio();
+  audioalli.src = "sound/crocodile.mp3";
+  audioalli.play();
+}
+
+function playmusic2(){
+  var audiowolf = new Audio();
+  audiowolf.src = "sound/WOLF_1.mp3";
+  audiowolf.play();
+}
+
+function playmusic3(){
+  var audiosnake = new Audio();
+  audiosnake.src = "sound/snake.mp3";
+  audiosnake.play();
+}
+function playmusic4(){
+  var audiolion = new Audio();
+  audiolion.src = "sound/LION1.mp3";
+  audiolion.play();
+}
+function playmusic5(){
+  var audiolion = new Audio();
+  audiolion.src = "sound/HYENAS.mp3";
+  audiolion.play();
+}
+function playmusic6(){
+  var audiolion = new Audio();
+  audiolion.src = "sound/Golfinho.mp3";
+  audiolion.play();
+}
+function playmusic7(){
+  var audioowl = new Audio();
+  audioowl.src = "sound/OWL.mp3";
+  audioowl.play();
+}
+  function playmusic8(){
+    var audioesel = new Audio();
+    audioesel.src = "sound/DONKEY.mp3";
+    audioesel.play();
+  
+}
+function playmusic9(){
+  var audiopferde = new Audio();
+  audiopferde.src = "sound/pferde.mp3";
+  audiopferde.play();
+
+}
+function playmusic10(){
+  var audioaffen = new Audio();
+  audioaffen.src = "sound/affen.mp3";
+  audioaffen.play();
+
+}
+function playmusic11(){
+  var audioelephant = new Audio();
+  audioelephant.src = "sound/elephant.mp3";
+  audioelephant.play();
+}
+function playmusic12(){
+  var audiopapagaien = new Audio();
+  audiopapagaien.src = "sound/galo.mp3";
+  audiopapagaien.play();
+}
+function playmusic13(){
+  var audioadler = new Audio();
+  audioadler.src = "sound/adler.mp3";
+  audioadler.play();
+}
+
+
